5c1cc2118360373a9c71658f30381fda63d6c5f1
das6521 <dsa6521@naver.com>
2019-11-29 17:38:19 +0900

    프로젝트 초기화

---
+++ epl/src/test/java/com/epl/EplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

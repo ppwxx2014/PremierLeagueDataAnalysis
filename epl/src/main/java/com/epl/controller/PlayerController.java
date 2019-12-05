2246232decc9ad47d6678223f59d13e0cc492051
das6521 <dsa6521@naver.com>
2019-12-05 18:26:39 +0900

    이상훈 커밋 1.player list 수정 -> 기존 thymeleaf 기법 데이터 연결 방식 삭제 ajax 방식으로 변경 				 -> 검색기능 오류 , 페이징오류 수정 2.playerContract insert 오류 수정 -> transferFee , BackNumber 데이터 입력 오류 수정

--- epl/src/main/java/com/epl/controller/PlayerController.java
+++ epl/src/main/java/com/epl/controller/PlayerController.java
@@ -1,12 +1,8 @@
 package com.epl.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.epl.service.interfaces.PlayerService;
 
@@ -22,21 +18,13 @@ public class PlayerController {
 		return "addPlayer";
 	};
 
-	// 플레이어 리스트 폼
 	@GetMapping("/getPlayerList")
-	public String getPlayerList(Model model, @RequestParam(value = "currentPage", defaultValue = "1") int currentPage,
-			@RequestParam(value = "searchWord", required = false) String searchWord) {
-		System.out.println("getPlayerList controller 폼 요청");
-		System.out.println("currentPage: " + currentPage); // 현 페이지수
-		System.out.println("SearchWord: " + searchWord);
-
-		int rowPerPage = 10; // 페이지당 보여줄 갯수
-
-		Map<String, Object> map = playerService.getPlayerListByPage(currentPage, rowPerPage, searchWord);
-		System.out.println("getPlayerList controller map :" + map);
-
-		model.addAttribute("map", map);
+	public String getPlayerList() {
+		System.out.println("getPlayerList 요청");
+		/*
+		 * List<Player> list = playerService.getPlayerList();
+		 * System.out.println("playerController @PostMapping list :" + list);
+		 */
 		return "getPlayerList";
 	}
-
 }
